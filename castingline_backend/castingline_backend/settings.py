50e523fc203d994368d321485cd95e2a54631f9c
Leslie <yyw0209_02@naver.com>
2026-01-24 08:08:54 +0900

    .

--- castingline_backend/castingline_backend/settings.py
+++ castingline_backend/castingline_backend/settings.py
@@ -75,8 +75,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
+    "http://116.125.120.94",
     "http://localhost",
-    "http://116.125.120.94"
+
 ]
 
 ROOT_URLCONF = "castingline_backend.urls"
